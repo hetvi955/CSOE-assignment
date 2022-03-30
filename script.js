eb8cce97df4ffdba094a4abd46263a42cc385114
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2022-03-30 21:05:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var num= document.getElementById("num");
+var data= 0;
+
+
+function increment( ){
+    data++;
+    num.innerText= data;
+}
+
+function decrement( ){
+    data--;
+    num.innerText=data;
+}
